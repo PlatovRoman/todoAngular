f333ab56ce449b00d4b2153f29d1edc1f325ef3e
PlatovRoman <43403807+PlatovRoman@users.noreply.github.com>
2020-11-19 21:29:32 +0300

    почти

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -19,14 +19,14 @@ export class HttpService {
 
   public postData(task: Task): void {
     this.http.post('http://127.0.0.1:3000/items', task)
-      .subscribe((newTask: Task) => this.responseServer.next(newTask));
+      .toPromise().then((newTask: Task) => this.responseServer.next(newTask));
   }
 
   public deleteData(id: number): void {
-    this.http.delete('http://127.0.0.1:3000/items/' + String(id)).subscribe();
+    this.http.delete('http://127.0.0.1:3000/items/' + String(id)).toPromise().then();
   }
 
   public putData(id: number, task: Task): void {
-    this.http.put('http://127.0.0.1:3000/items/' + String(id), task).subscribe();
+    this.http.put('http://127.0.0.1:3000/items/' + String(id), task).toPromise().then();
   }
 }
